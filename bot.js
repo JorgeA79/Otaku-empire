c0f82a5ae1850f7cc5fe7f814a721573d8fc5c8b
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2019-04-26 23:30:44 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,19 @@
+    
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
 client.on('ready',() => {
 	//Edit after game so u can add wut game bot is playing between "" xd
 	   client.user.setPresence({game: {name: "with Sel | a!help", type: 0}});
 });
-var lastvideoxd = "https://www.youtube.com/watch?v=EpUMsBeDy1g"
 
-client.on('message', message => {
-	if (message.author.bot){
-	if (message.content.startsWith('https://www.youtube')) {
-		lastvideoxd = (message.content)
-		var lastvideo = (message.content)
-	message.delete(1000);
-		 message.channel.send(`@everyone WHAPAM \n ` + lastvideo);
-	}}
-});
+
+    for(var i = 0; i < message.embeds.length; i++) {
+        if(message.embeds[i].title.includes("Bungou Stray") || message.embeds[i].title.includes("Bungou Stray")) {
+      message.channel.send("im gay");
+    }
+}
+
 //Important
 client.login(process.env.BOT_TOKEN);
