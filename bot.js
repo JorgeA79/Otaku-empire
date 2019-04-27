9b2f7e07a7904a85c55f90c4f6863e0219ea4d68
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2019-04-27 00:20:36 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,9 +11,9 @@ client.on('ready',() => {
 
 client.on('message', message => {
 	if (message.author.bot){
-	if(message.content.toLowerCase().includes('cyber'))
+	if(message.content.toLowerCase().includes('cyber')){
     message.channel.send("Key Word Detected ");
-else {
+} else {
     for(var i = 0; i < message.embeds.length; i++) {
         if(message.embeds[i].title.includes("Bungo Stray")) {
             message.channel.send("Detected");
