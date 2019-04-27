c90ee480c41701d1f40bb74286eb9bfc2b425ba8
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2019-04-27 00:46:36 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,17 +9,7 @@ client.on('ready',() => {
 });
 
 
-client.on('message', message => {
-	if (message.author.bot){
-	if(message.content.toLowerCase().includes('cyber')){
-    message.channel.send("Key Word Detected ");
-} else {
-    for(var i = 0; i < message.embeds.length; i++) {
-        if(message.embeds[i].title.includes("Bungo Stray")) {
-            message.channel.send("Detected");
-            
-    }
-}}});
+
 
 //Important
 client.login(process.env.BOT_TOKEN);
