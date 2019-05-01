8d79e646b14b70f4b46969ba4cb5c699b6782190
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2019-04-30 19:19:58 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,10 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+var prefix = "y!"
+var BOTID = "0"
+var CHANNELID = "0"
+
 
 client.on('ready',() => {
 	//Edit after game so u can add wut game bot is playing between "" xd
@@ -10,10 +14,22 @@ client.on('ready',() => {
 
 
 client.on('message', message => {
+	if (message.author === client.user) return;
+	if (message.content.startsWith(prefix + 'setchannel')) {
+		
+	 let command = message.content.substring(message.content.indexOf(" ") + 1, message.content.length);
+		 message.channel.send('' + command);
+	}
+});
+
+client.on('message', message => {
+	
+ 	if (message.channel.id === CHANNEL_ID)){
 	if (message.author.bot){
 	if(message.author.id === "125367104336691200") {
+		
 		message.channel.send(`@everyone`);
-	}}
+	}}}
 });
 
 //Important
