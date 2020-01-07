187028923a5e30ba8f57797721809136566a301a
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-01-07 16:37:09 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,13 +20,13 @@ client.on('message', message => {
 
 client.on('message', message => {
 	
- 	if (message.channel.id === "570718104057806848"){
+ 	if (message.channel.id === "664219562149019691"){
 	if (message.author.bot){
 	if(message.author.id === "125367104336691200") {
 		
 	
 		
-		message.channel.send(`<@&572950228177649664>, New episode!`);
+		message.channel.send(`<@&664222622057234432>, New episode!`);
 	}}}
 });
 
